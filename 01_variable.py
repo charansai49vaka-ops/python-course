c543eb400d290dfa34f303f3bd890e7f88016013
Sai Charan vaka <charansai49vaka@gmail.com>
2026-02-13 21:21:45 +0530

    variables

---
+++ 01_variable.py
@@ -0,0 +1,18 @@
+age=3
+print(age)
+print(type(age))
+
+
+name="sai charan"
+print(name)
+print(type(name))
+
+
+cgpa=8.51
+print(cgpa)
+print(type(cgpa))
+
+
+bankai= True
+print(bankai)
+print(type(bankai))
